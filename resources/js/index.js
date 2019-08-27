25d5117b475ef3f038a30df05d3a24b8f8fe6799
jstran9 <wptran58@gmail.com>
2019-08-27 01:56:37 -0700

    creating a link and route to the blog creation form.

--- resources/js/index.js
+++ resources/js/index.js
@@ -4,8 +4,13 @@ import Blog from "./components/Blog";
 import BlogArticle from "./components/BlogArticle";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import Example from "./components/Example";
+import BlogArticlePost from "./components/BlogArticlePost";
 
 export default class Index extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div className="container">
@@ -13,6 +18,7 @@ export default class Index extends Component {
                     <div>
                         <Link to="/">Home</Link>
                         <Link to="/blog">Blog</Link>
+                        <Link to="/blog/post-article">Post Article</Link>
 
                         <Route exact path="/" component={Example} />
                         <Route exact path="/blog" component={Blog} />
@@ -20,6 +26,11 @@ export default class Index extends Component {
                             exact
                             path="/blog/:id"
                             render={props => <BlogArticle {...props} />}
+                        />
+                        <Route
+                            exact
+                            path="/blog/post-article"
+                            component={BlogArticlePost}
                         />
                     </div>
                 </Router>
