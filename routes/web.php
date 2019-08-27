395048dbe6fb7c929549d37826a33eba55f5aa93
jstran9 <wptran58@gmail.com>
2019-08-26 22:21:29 -0700

    grabbing content from the database and displaying it.

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $blogArticle = DB::table('blogs')->get(); // get all rows from the database.
+    // return view('welcome');
+    return $blogArticle;
 });
